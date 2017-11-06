ba7b1d7802824abe711f6707f402e8266509915e
Eric McKinney <mckinney99@gmail.com>
2017-11-06 16:41:52 +0000

    automatically backed up by learn

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -1,11 +1,16 @@
 class CashRegister
   attr_accessor :items, :discount, :total, :last_transaction
 
-  def initialize(total)
+  def initialize(discount = 0)
     @total = 0
     @items = []
     @discount = discount.to_f
-
   end
 
+  def item(title, price, quantity = 1)
+    self.total = total+(price*quantity)
+    quantity.times do self.items << title
+    end
+    self.last_transaction = price*quantity
+  end
 end
